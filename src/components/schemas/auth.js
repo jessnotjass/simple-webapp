856970b7481a4ba4ce67fe6b65b67a67e9111e84
Jess Virgil Lanchinebre <jevi.lanchinebre@gmail.com>
2020-02-14 20:14:21 -0800

    refactor: auth schema

--- src/components/schemas/auth.js
+++ src/components/schemas/auth.js
@@ -1,31 +1,28 @@
 import * as Yup from 'yup'
 
+const firstName = Yup.string()
+  .required('Required!')
+const lastName = Yup.string()
+  .required('Required!')
+const email = Yup.string()
+  .email('Invalid email!')
+  .required('Required!')
+const password = Yup.string()
+  .min(6, 'Must be atleast 6 characters!')
+  .matches(
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+    'Must contain a lowercase letter, uppercase letter, number & a one of the following symbols !@#$&*'
+  )
+  .required('Required!')
+
 export const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .required('Required!'),
-  lastName: Yup.string()
-    .required('Required!'),
-  email: Yup.string()
-    .email('Invalid email!')
-    .required('Required!'),
-  password: Yup.string()
-    .min(6, 'Must be atleast 6 characters!')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
-      'Must contain a lowercase letter, uppercase letter, number & a one of the following symbols !@#$&*'
-    )
-    .required('Required!')
+  firstName,
+  lastName,
+  email,
+  password
 })
 
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email!')
-    .required('Required!'),
-  password: Yup.string()
-    .min(6, 'Must be atleast 6 characters!')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
-      'Must contain a lowercase letter, uppercase letter, number & a one of the following symbols !@#$&*'
-    )
-    .required('Required!')
+  email,
+  password
 })
